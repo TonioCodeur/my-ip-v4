acf9521639181aaeb412408c7a462d499534a2a7
Antonin MICHEL <a.copiercoller@gmail.com>
2025-11-07 06:34:38 +0100

    Modification des metadata, changelent du titre de la page

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,21 +1,21 @@
-import React from 'react'
-import { I18nProviderClient } from '../../../locales/client'
-import { QueryProvider } from '@/components/providers/query-provider'
-import { ModeToggle } from '@/components/mode-toggle'
-import { LanguageSwitcher } from '@/components/language-switcher'
+import React from "react";
+import { I18nProviderClient } from "../../../locales/client";
+import { QueryProvider } from "@/components/providers/query-provider";
+import { ModeToggle } from "@/components/mode-toggle";
+import { LanguageSwitcher } from "@/components/language-switcher";
 
 export function generateStaticParams() {
-  return [{ locale: 'en' }, { locale: 'fr' }]
+  return [{ locale: "en" }, { locale: "fr" }];
 }
 
 export default async function LocaleLayout({
   children,
   params,
 }: {
-  children: React.ReactNode
-  params: Promise<{ locale: string }>
+  children: React.ReactNode;
+  params: Promise<{ locale: string }>;
 }) {
-  const { locale } = await params
+  const { locale } = await params;
 
   return (
     <I18nProviderClient locale={locale}>
@@ -27,5 +27,5 @@ export default async function LocaleLayout({
         </div>
       </QueryProvider>
     </I18nProviderClient>
-  )
+  );
 }
